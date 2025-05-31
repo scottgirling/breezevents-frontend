99ea00fa4bf13bb451f99d7062b604b014c03c06
scottgirling <scottgirling0812@gmail.com>
2025-05-31 18:07:01 +0100

    amend event list styling

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,11 +4,14 @@ import { useEffect, useState } from 'react';
 import { fetchEvents, fetchTags } from '../../utils/api';
 
 export const Home = () => {
+    const [loading, setLoading] = useState(true);
     const [tags, setTags] = useState([]);
     const [eventTag, setEventTag] = useState("");
+    const [activeEventTag, setActiveEventTag] = useState("View all");
     const [events, setEvents] = useState([]);
 
     useEffect(() => {
+        setLoading(true);
         fetchTags()
         .then((returnedTags) => {
             setTags(returnedTags);
@@ -16,14 +19,14 @@ export const Home = () => {
         fetchEvents(eventTag)
         .then((returnedEvents) => {
             setEvents(returnedEvents)
+            setLoading(false);
         })
     }, [eventTag]);
-
 
     return (
         <>
             <h2 className="home-title">Discover your next <span className="italic-green">unforgettable</span> event today</h2>
-            <p className="join">Join <span className="italic-green">breez<span className="bold-e">e</span>vents</span> to effortlessly find and book awesome events. Experience the joy of seamless planning.</p>
+            <p className="home-subheading">Join <span className="italic-green">breez<span className="bold-e">e</span>vents</span> to effortlessly find and book awesome events. Experience the joy of seamless planning.</p>
             <section className="home-buttons">
                 <Link to="/account">
                     <button className="btn btn-green">
@@ -34,25 +37,23 @@ export const Home = () => {
                     Explore
                 </button>
             </section>
-            <section>
+            <section className="sub-section">
                 <img src="../../../home-top.jpg"/>
-            </section>
-            <section className="features">
                 <h3>Discover Our Exciting Event Features</h3>
                 <p>Experience seamless event booking with our user-friendly platform.</p>
                 <img src="../../../home-features.jpg"/>
             </section>
-            <section className="effortlessly">
+            <section className="sub-section">
                 <h3>Effortlessly Book Your Next Event Today</h3>
                 <p><span className="italic-green">breez<span className="bold-e">e</span>vents</span> makes event booking a breeze. Unparalleled convenience allows you to book events with just a few clicks.</p>
                 <img src="../../../home-effortlessly.jpg" />
             </section>
-            <section className="variety">
+            <section className="sub-section">
                 <h3>Explore a Variety of Events</h3>
                 <p>Choose from a wide range of events tailored to your interests.</p>
                 <img src="../../../home-variety.jpg" />
             </section>
-            <section className="payments">
+            <section className="sub-section">
                 <h3>Secure Payments for Peace of Mind</h3>
                 <p>Enjoy safe and reliable payment options for your bookings.</p>
                 <img src="../../../home-payments.jpg" />
@@ -67,48 +68,69 @@ export const Home = () => {
                     Explore
                 </button>
             </section>
-            <section className="discover">
+            <section className="sub-section">
                 <h3>Discover Events</h3>
                 <p>Explore upcoming events are secure your spot today!</p>
             </section>
 
             <section className="discover-events">
                 <ul className="tags-list">
-                    <li className="tag-name">View all</li>
+                    <li className="tag-name">
+                        <button className="btn btn-tag" onClick={() => {
+                            setEventTag("");
+                            setActiveEventTag("View all");
+                        }}>
+                            View all
+                            {activeEventTag === "View all" && <p className="underline"></p>}</button>
+                    </li>
                     {tags.map((tag) => {
                         return (
-                            <>
-                                <li className="tag-name" key={tag.tag_id}>{tag.name}</li>
-                            </>
+                            <li className="tag-name" key={tag.tag_id}>
+                                <button className="btn btn-tag" value={tag.slug} onClick={(event) => {
+                                    setEventTag(event.target.value);
+                                    setActiveEventTag(tag.name);
+                                }}>
+                                    {tag.name}
+                                    {tag.name === activeEventTag && <p className="underline"></p>}
+                                </button>
+                            </li>
                         )
                     })}
                 </ul>
             </section>
 
             <section>
-                <ul>
-                    {events.map((event) => {
-                        return (
-                            <>
+                {loading ? (
+                    <p className="events-loading">Loading events...</p>
+                ) : (
+                    events.length ? (
+                        <ul>
+                        {events.map((event) => {
+                            return (
                                 <li className="event-card" key={event.event_id}>
-                                    <h4 className="event-title">{event.title}</h4>
-                                    <p className="event-overview">{event.event_overview}</p>
-                                    <section className="event-price-date">
-                                        {event.price !== 0 ? (
-                                            <p>£{event.price}</p>
-                                        ) : (
-                                            <p className="free-ticket">FREE</p>
-                                        )}
-                                        <p>{new Date(event.start_time).toDateString()}</p>
-                                        <button className="btn btn-white">
-                                            Find Tickets
-                                        </button>
-                                    </section>
-                                </li>
-                            </>
-                        )
-                    })}
-                </ul>
+                                        <h4 className="event-title">{event.title}</h4>
+                                        <p className="event-overview">{event.event_overview}</p>
+                                        <section className="event-price-date">
+                                            {event.price !== 0 ? (
+                                                <p className="ticket-price">£{event.price}</p>
+                                            ) : (
+                                                <p className="free-ticket">FREE</p>
+                                            )}
+                                            <p className="event-date">{new Date(event.start_time).toDateString()}</p>
+                                            <Link to={`/events/${event.event_id}`}>
+                                                <button className="btn btn-white">
+                                                    Find Tickets
+                                                </button>
+                                            </Link>
+                                        </section>
+                                    </li>
+                                )
+                            })}
+                            </ul>
+                        ) : (
+                            <p className="no-events-found">No events found. Please select another category.</p>
+                    )
+                )}
             </section>
         </>
     )
