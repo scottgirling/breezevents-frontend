e27b2eb2f768208953a6ed136c3aaf997d6b4623
scottgirling <scottgirling0812@gmail.com>
2025-06-02 13:20:52 +0100

    add host details to event and setup venue details ready for api

--- src/components/single-event/SingleEvent.jsx
+++ src/components/single-event/SingleEvent.jsx
@@ -1,19 +1,35 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { fetchEventById } from "../../utils/api";
+import { fetchEventById, fetchHostById } from "../../utils/api";
 import "./SingleEvent.css";
 
 export const SingleEvent = () => {
     const { event_id } = useParams();
     const [event, setEvent] = useState({});
-    const [showDescription, setShowDescription] = useState(false);
+    const [venue, setVenue] = useState({});
+    const [host, setHost] = useState({});
+    const [showEventDescription, setShowEventDescription] = useState(false);
+    const [showVenueDetails, setShowVenueDetails] = useState(false);
+    const [showHostDetails, setShowHostDetails] = useState(false);
 
     useEffect(() => {
         fetchEventById(event_id)
         .then((returnedEvent) => {
             setEvent(returnedEvent);
         })
-    }, [])
+        // .then(() => {
+        //     fetchVenueById(event.venue_id)
+        // })
+        // .then((returnedVenue) => {
+        //     setVenue(returnedVenue);
+        // })
+        .then(() => {
+            return fetchHostById(6)
+        })
+        .then((returnedHost) => {
+            setHost(returnedHost)
+        })
+    }, []);
 
     return (
         <>
@@ -22,22 +38,57 @@ export const SingleEvent = () => {
             <img className="event-image" src={event.event_image_url} />
             <section className="event-info">
                 <p className="event-start-time">{new Date(event.start_time).toDateString()}</p>
-                <p className="event-price">£{event.price}</p>
+                {event.price !== 0 ? (
+                    <p className="ticket-price">£{event.price}</p>
+                ) : (
+                    <p className="free-ticket">FREE</p>
+                )}
                 <button className="btn-buy-tickets">Buy Tickets</button>
             </section>
-            <section className="event-description">
+            <section className="event-description-bar">
                 <p>Description</p>
-                <button onClick={() => setShowDescription(!showDescription)}>
-                    {!showDescription ? (
+                <button onClick={() => setShowEventDescription(!showEventDescription)}>
+                    {!showEventDescription ? (
                         <i className="fa-solid fa-angle-down"></i>
                     ) : (
                         <i className="fa-solid fa-angle-up"></i>
                     )}
                 </button>
             </section>
-            {showDescription && (
-                <p>{event.description}</p>
-            )}
+            <section>
+                {showEventDescription && (
+                    <p className="text-showing">{event.description}</p>
+                )}
+            </section>
+            <section className="venue-bar">
+                <p>Venue</p>
+                <button onClick={() => setShowVenueDetails(!showVenueDetails)}>
+                    {!showVenueDetails ? (
+                        <i className="fa-solid fa-angle-down"></i>
+                    ) : (
+                        <i className="fa-solid fa-angle-up"></i>
+                    )}
+                </button>
+            </section>
+            <section className="host-bar">
+                <p>Host</p>
+                <button onClick={() => setShowHostDetails(!showHostDetails)}>
+                    {!showHostDetails ? (
+                        <i className="fa-solid fa-angle-down"></i>
+                    ) : (
+                        <i className="fa-solid fa-angle-up"></i>
+                    )}
+                </button>
+            </section>
+            <section>
+                {showHostDetails && (
+                    <>
+                        <p className="text-showing">{event.title} is hosted by <span className="host-info">{host.name}.</span></p>
+                        <p className="text-showing">A bit about {host.name}: <span className="host-info">{host.bio}</span></p>
+                        <p className="text-showing">Should you need to contact {host.name}: <span className="host-info">{host.email}</span></p>
+                    </>
+                )}
+            </section>
         </>
     )
 }
