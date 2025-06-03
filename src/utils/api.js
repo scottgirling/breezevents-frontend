750603f141a11496c8162dd4607cb0df3e594deb
scottgirling <scottgirling0812@gmail.com>
2025-06-03 16:22:42 +0100

    remove unused apis

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,18 +1,18 @@
 import axios from "axios";
 
 const eventsApi = axios.create({
-    baseURL: "https://events-platform-be-1fmx.onrender.com/api/"
+    baseURL: "https://events-platform-be-1fmx.onrender.com/api"
 });
 
 export const fetchTags = () => {
-    return eventsApi.get("tags")
+    return eventsApi.get("/tags")
     .then((response) => {
         return response.data.tags;
     });
 }
 
 export const fetchEvents = (tag) => {
-    return eventsApi.get("events", {
+    return eventsApi.get("/events", {
         params: {
             tag: tag
         }
@@ -23,22 +23,8 @@ export const fetchEvents = (tag) => {
 }
 
 export const fetchEventById = (event_id) => {
-    return eventsApi.get(`events/${event_id}`)
+    return eventsApi.get(`/events/${event_id}`)
     .then((response) => {
         return response.data.event;
     });
-}
-
-// export const fetchVenueById = (venue_id) => {
-//     return eventsApi.get(`venues/${venue_id}`)
-//     .then((response) => {
-//         return response.data.venue;
-//     });
-// }
-
-export const fetchHostById = (host_id) => {
-    return eventsApi.get(`users/${host_id}`)
-    .then((response) => {
-        return response.data.user;
-    })
 }
