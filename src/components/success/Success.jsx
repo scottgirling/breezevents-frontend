2410489d0c07a821f8b0b13d6bb643a8e204a8ec
scottgirling <scottgirling0812@gmail.com>
2025-06-05 17:31:02 +0100

    add Success component

--- src/components/success/Success.jsx
+++ src/components/success/Success.jsx
@@ -1,5 +1,50 @@
+import { useSearchParams } from "react-router-dom";
+import { fetchSessionData } from "../../utils/api";
+import { useEffect, useState } from "react";
+import "./Success.css";
+
 export const Success = () => {
+    const [customer, setCustomer] = useState({});
+    const [event, setEvent] = useState({});
+    const [searchParams] = useSearchParams();
+
+    useEffect(() => {
+        const sessionID = searchParams.get("session_id");
+        fetchSessionData(sessionID)
+        .then((returnedData) => {
+            setCustomer(returnedData.customer_details);
+            setEvent(JSON.parse(returnedData.metadata.eventDetails));
+        });
+    }, []);
+
+    console.log(event)
+
     return (
-        <p>Success!</p>
+        <section className="success">
+            <section>
+                {customer.name ? (
+                    <h1 className="confirmation">Booking confirmed, {customer.name}!</h1>
+                ) : (
+                    <h1 className="confirmation">Booking Confirmed!</h1>
+                )}
+                <p>You're going to: <span className="session-info">{event.title}</span></p>
+                <p>A confirmation email will be sent to <span className="session-info">{customer.email}</span></p>
+            </section>
+            <section className="calendar">
+                <section>
+                    <i className="fa-regular fa-calendar-check"></i>
+                    <p>{new Date(event.start_time).toDateString()}</p>
+                </section>
+                <button>
+                    Add to Google Calendar
+                </button>
+            </section>
+
+            <section>
+                <button>
+                    View My Events
+                </button>
+            </section>
+        </section>
     )
 }
