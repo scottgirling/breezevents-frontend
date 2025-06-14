7985349db1e4cb09a7799196fa8d7198126ffff1
scottgirling <scottgirling0812@gmail.com>
2025-06-14 16:01:33 +0100

    add logic to handle updateUser functionality

--- src/components/user-profile/UserProfile.jsx
+++ src/components/user-profile/UserProfile.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom"
-import { fetchUserById } from "../../utils/api";
+import { fetchUserById, updateUser } from "../../utils/api";
 import { supabase } from "../../supabase/client";
 import { AttendeeEvents } from "./attendee-profile/AttendeeEvents";
 import { HostEvents } from "./host-profile/HostEvents";
@@ -9,9 +9,12 @@ import "./UserProfile.css";
 export const UserProfile = () => {
     const navigate = useNavigate();
     const { user_id } = useParams();
-
     const [loading, setLoading] = useState(true);
     const [user, setUser] = useState({});
+    const [isDisabled, setIsDisabled] = useState(true);
+    const [userDetails, setUserDetails] = useState({});
+    const [updateSuccess, setUpdateSuccess] = useState(null);
+    const [discardSuccess, setDiscardSuccess] = useState(null);
 
     useState(() => {
         setLoading(true);
@@ -22,15 +25,60 @@ export const UserProfile = () => {
         })
     }, []);
 
+    const handleUserDetailsClassName = () => {
+        let userProfileForm = "form-disabled";
+        if (!isDisabled) {
+            userProfileForm = "form-not-disabled";
+        }
+        return userProfileForm;
+    }
+
+    const handleUserProfileUpdate = (event) => {
+        const key = event.target.name;
+        const value = event.target.value;
+        setUserDetails({
+            ...userDetails,
+            [key]: value
+        })
+    }
+
+    const isDiscardButtonDisabled = () => {
+        let userProfileDiscardButton = "discard-button-disabled";
+        if (Object.keys(userDetails).length) {
+            userProfileDiscardButton = "discard-changes"
+        }
+        return userProfileDiscardButton;
+    }
+
+    const isSubmitButtonDisabled = () => {
+        let userProfileSubmitButton = "submit-button-disabled";
+        if (Object.keys(userDetails).length) {
+            userProfileSubmitButton = "save-changes"
+        }
+        return userProfileSubmitButton;
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        updateUser(user_id, userDetails)
+        .then(() => {
+            setUpdateSuccess("Successfully updated your profile!")
+            setIsDisabled(true);
+            setUserDetails({});
+        });
+    }
+
     const handleSignOut = async () => {
-        const { error } = await supabase.auth.signOut();
-        navigate("/account")
+        supabase.auth.signOut()
+        .then(() => {
+            navigate("/account")
+        });
     }
 
     if (loading) {
         return (
             <p className="loading">Loading your profile...</p>
-        )
+        );
     }
 
     return (
@@ -56,6 +104,12 @@ export const UserProfile = () => {
                     <section className="your-profile-button">
                         <button 
                             className="your-profile-button"
+                            onClick={() => {
+                                setIsDisabled(!isDisabled);
+                                setUpdateSuccess(null);
+                                setDiscardSuccess(null);
+                                setUserDetails({});
+                            }}
                         >
                             <i className="fa-solid fa-user-pen"></i>
                             <p>Edit Profile</p>
@@ -66,19 +120,97 @@ export const UserProfile = () => {
                     <form className="user-profile-form">
                         <section>
                             <label htmlFor="name">Name:</label>
-                            <input type="text" id="name" name="name" placeholder={user.name}></input>
+                            <input 
+                                type="text" 
+                                id="name" 
+                                name="name" 
+                                defaultValue={user.name} 
+                                disabled={isDisabled} 
+                                className={handleUserDetailsClassName()}
+                                onBlur={(event) => handleUserProfileUpdate(event)}
+                            >
+                            </input>
                         </section>
                         <section>
                             <label htmlFor="username">Username:</label>
-                            <input type="text" id="username" name="username" placeholder={user.username}></input>
+                            <input 
+                                type="text" 
+                                id="username" 
+                                name="username" 
+                                defaultValue={user.username} 
+                                disabled={isDisabled} 
+                                className={handleUserDetailsClassName()}
+                                onBlur={(event) => handleUserProfileUpdate(event)}
+                            >
+                            </input>
                         </section>
                         <section>
                             <label htmlFor="email">Email:</label>
-                            <textarea type="text" id="email" name="email" placeholder={user.email}></textarea>
+                            <input 
+                                type="text" 
+                                id="email" 
+                                name="email" 
+                                defaultValue={user.email} 
+                                disabled={isDisabled} 
+                                className={handleUserDetailsClassName()}
+                                onBlur={(event) => handleUserProfileUpdate(event)}
+                            >
+                            </input>
                         </section>
                         <section>
                             <label htmlFor="bio">Bio:</label>
-                            <textarea type="text" id="bio" name="bio" placeholder={user.bio}></textarea>
+                            <textarea 
+                                type="text" 
+                                id="bio" 
+                                name="bio" 
+                                defaultValue={user.bio} 
+                                disabled={isDisabled} 
+                                className={handleUserDetailsClassName()}
+                                onBlur={(event) => handleUserProfileUpdate(event)}
+                            >
+                            </textarea>
+                        </section>
+                        <section >
+                            <label htmlFor="avatar_url">Avatar URL:</label>
+                            <input 
+                                type="text" 
+                                id="avatar_url" 
+                                name="avatar_url" 
+                                defaultValue={user.avatar_url} 
+                                disabled={isDisabled}
+                                className={handleUserDetailsClassName()}
+                                onBlur={(event) => handleUserProfileUpdate(event)}
+                            >
+                            </input>
+                        </section>
+
+                        <section className="user-profile-buttons">
+                            <button 
+                                disabled={!Object.keys(userDetails).length}
+                                className={isDiscardButtonDisabled()}
+                                onClick={() => {
+                                    setDiscardSuccess("Successfully discarded changes!");
+                                    setIsDisabled(true);
+                                }}
+                                type="reset"
+                            >
+                                Discard Changes
+                            </button>
+                            <button
+                                disabled={!Object.keys(userDetails).length}
+                                className={isSubmitButtonDisabled()}
+                                onClick={(event) => handleSubmit(event)}
+                            >
+                                Save Changes
+                            </button>
+                        </section>
+                        <section>
+                            {updateSuccess && (
+                                <p className="update-success">{updateSuccess}</p>
+                            )}
+                            {/* {discardSuccess && (
+                                <p className="update-success">{discardSuccess}</p>
+                            )} */}
                         </section>
                     </form>
                 </section>
@@ -88,7 +220,6 @@ export const UserProfile = () => {
                         Sign Out
                     </button>
                 </section>
-
             </section>
         </section>
     )
