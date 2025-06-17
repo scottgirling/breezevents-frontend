1b96be618aa7d13aaa26a6802affeb4506746f03
scottgirling <scottgirling0812@gmail.com>
2025-06-17 17:54:53 +0100

    add Error component to handle unauthorised routes

---
+++ src/components/error/Error.jsx
@@ -0,0 +1,38 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../contexts/AuthProvider";
+import "./Error.css";
+
+export const Error = () => {
+    const { loggedInUser } = useAuth();
+    const navigate = useNavigate();
+
+    return (
+        <section className="error">
+            <i className="fa-solid fa-circle-exclamation"></i>
+            <p>Oops... it looks like you're trying to visit a page you are not authorised to access.</p>
+            <section className="error-redirect-buttons">
+                <button 
+                    id="error-button-home"
+                    onClick={() => navigate("/")}
+                >
+                    Home
+                </button>
+                {loggedInUser.id ? (
+                    <button 
+                        id="error-button-account"
+                        onClick={() => navigate(`/breezer/${loggedInUser.id}`)}
+                    >
+                        Your Account
+                    </button>
+                ) : (
+                    <button 
+                        id="error-button-account"
+                        onClick={() => navigate("/account")}
+                    >
+                        Sign In
+                    </button>
+                )}
+            </section>
+        </section>
+    )
+}
