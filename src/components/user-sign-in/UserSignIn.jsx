6c52d4c3e983013d6c5389383b27aba46395d1e4
scottgirling <scottgirling0812@gmail.com>
2025-07-10 20:01:12 +0100

    add signInWithOAuth (Google) logic

--- src/components/user-sign-in/UserSignIn.jsx
+++ src/components/user-sign-in/UserSignIn.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { addUser, fetchUserById } from "../../utils/api";
 import { useAuth } from "../../contexts/AuthProvider";
@@ -13,6 +13,10 @@ export const UserSignIn = () => {
     const [email, setEmail] = useState(null);
     const [password, setPassword] = useState(null);
     const [signInError, setSignInError] = useState(null);
+
+    useEffect(() => {
+        getUserData();
+    }, []);
 
     const handleSignUp = (event) => {
         event.preventDefault();
@@ -70,46 +74,87 @@ export const UserSignIn = () => {
         }
     }
 
+    const handleGoogleSignIn = async () => {
+        await supabase.auth.signInWithOAuth({
+            provider: "google",
+            options: {
+                redirectTo: "http://localhost:5173/account"
+            }
+        });
+    }
+
+    const getUserData = async () => {
+        const { data: { session } } = await supabase.auth.getSession();
+
+        if (session.user.aud === "authenticated") {
+            const userDetails = {}
+    
+            userDetails.user_id = session.user.id;
+            userDetails.name = session.user.user_metadata.name;
+            userDetails.username = "";
+            userDetails.email = session.user.email;
+            userDetails.role = "attendee";
+
+            try {
+                const returnedUser = await fetchUserById(session.user.id)
+                
+                if (returnedUser) {
+                    navigate(`/breezer/${session.user.id}`);
+                } 
+            } catch {
+                addUser(userDetails)
+                .then(()=> {
+                    navigate(`/breezer/${session.user.id}`);
+                });
+            };
+        }
+    }
+
     return (
         <section className="sign-in-comp">
             {userAction === "Sign In" ? (
-                <section>
-                    <h1>Sign in to your account</h1>
-                    <form 
-                        className="sign-up"
-                        onSubmit={(event) => handleSignIn(event)}
-                    >
-                        <label htmlFor="email">Email Address</label>
-                        <input 
-                            onChange={(event) => setEmail(event.target.value)} 
-                            type="text" 
-                            id="email" 
-                            name="email" 
-                            placeholder="Your email address"
+                <>
+                    <section>
+                        <button onClick={() => handleGoogleSignIn()}><i className="fa-brands fa-google"></i> Continue with Google</button>
+                    </section>
+                    <section>
+                        <h1>Sign in to your account</h1>
+                        <form 
+                            className="sign-up"
+                            onSubmit={(event) => handleSignIn(event)}
                         >
-                        </input>
-                        
-                        <label htmlFor="password">Password</label>
-                        <input 
-                            onChange={(event) => setPassword(event.target.value)} 
-                            type="password" 
-                            id="password" 
-                            name="password" 
-                            placeholder="Your password"
-                        >
-                        </input>
-                        {signInError && (
-                            <p className="sign-in-error">{signInError}</p>
-                        )}
-                        <button type="submit">
-                            Sign In
-                        </button>
-                    </form>
-                    <button 
-                        onClick={() => setUserAction("Sign Up")}
-                        className="switch-mode"
-                    >Don't have an account? Sign up.</button>
-                </section>
+                            <label htmlFor="email">Email Address</label>
+                            <input 
+                                onChange={(event) => setEmail(event.target.value)} 
+                                type="text" 
+                                id="email" 
+                                name="email" 
+                                placeholder="Your email address"
+                            >
+                            </input>
+                            
+                            <label htmlFor="password">Password</label>
+                            <input 
+                                onChange={(event) => setPassword(event.target.value)} 
+                                type="password" 
+                                id="password" 
+                                name="password" 
+                                placeholder="Your password"
+                            >
+                            </input>
+                            {signInError && (
+                                <p className="sign-in-error">{signInError}</p>
+                            )}
+                            <button type="submit">
+                                Sign In
+                            </button>
+                        </form>
+                        <button 
+                            onClick={() => setUserAction("Sign Up")}
+                            className="switch-mode"
+                        >Don't have an account? Sign up.</button>
+                    </section>
+                </>
             ) : (
                 <section>
                     <h1>Create an account</h1>
