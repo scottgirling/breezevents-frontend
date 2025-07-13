b549380bacb63baada5cd808c33589bb6138009f
scottgirling <scottgirling0812@gmail.com>
2025-07-13 18:32:51 +0100

    refactor CSS to Tailwind CSS for auth-confirm component

--- src/components/auth-confirm/AuthConfirm.jsx
+++ src/components/auth-confirm/AuthConfirm.jsx
@@ -1,12 +1,11 @@
 import { Link } from "react-router-dom";
-import "./AuthConfirm.css";
 
 export const AuthConfirm = () => {
     return (
-        <section className="auth-confirm">
+        <section className="mt-8 min-h-[60vh] sm:min-h-[70vh]">
             <h1>Email confirmed!</h1>
             <Link to="/account">
-                <button className="btn btn-salmon">
+                <button className="btn btn-salmon m-4">
                     Sign In
                 </button>
             </Link>
