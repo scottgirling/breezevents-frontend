8c8715f739126fd1e045872906e4febacacd81d8
scottgirling <scottgirling0812@gmail.com>
2025-07-14 18:50:40 +0100

    refactor CSS to Tailwind CSS for Header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,32 +6,35 @@ export const Header = () => {
     const { loggedInUser } = useAuth();
 
     return (
-        <section className="header">
+        <section className="flex items-center justify-between 2xl:mx-20">
             <Link to="/">
-                <h1 className="breezevents">breez<span className="bold-e">e</span>vents</h1>
+                <h1 className="text-[#317575] text-3xl my-4 font-normal">breez<span className="bold-e">e</span>vents</h1>
             </Link>
             
             {loggedInUser.name ? (
                 <Link 
-                className="user-signin" 
-                to={`breezer/${loggedInUser.id}`}
-                aria-label="View your account">
-                    <p className="fa-user">{loggedInUser.name[0].toUpperCase()}</p>
+                    className="user-signin" 
+                    to={`breezer/${loggedInUser.id}`}
+                    aria-label="View your account"
+                >
+                    <p className="fa-user text-[#317575]">{loggedInUser.name[0].toUpperCase()}</p>
                 </Link>
             ) : (
                 loggedInUser.id ? (
                     <Link 
-                    className="user-signin" 
-                    to={`breezer/${loggedInUser.id}`}
-                    aria-label="View your account">
-                        <i className="fa-solid fa-user"></i>
+                        className="user-signin" 
+                        to={`breezer/${loggedInUser.id}`}
+                        aria-label="View your account"
+                    >
+                        <i className="fa-solid fa-user text-[#317575]"></i>
                     </Link>
                 ) : (
                     <Link 
-                    className="user-signin" 
-                    to="/account"
-                    aria-label="Sign in to your account">
-                        <i className="fa-solid fa-user"></i>
+                        className="user-signin" 
+                        to="/account"
+                        aria-label="Sign in to your account"
+                    >
+                        <i className="fa-solid fa-user text-[#317575]"></i>
                     </Link>
                 )
             )}
